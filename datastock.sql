e8ac5546f239e663277686ca9ac860703e1eb8a7
Christian <lowechristian1@yahoo.fr>
2018-08-25 10:53:07 +0100

    création des tables famille, sous famille et article dans la BD

--- datastock.sql
+++ datastock.sql
@@ -16,6 +16,36 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`database` /*!40100 DEFAULT CHARACTER SE
 
 USE `database`;
 
+/*Table structure for table `tbarticle` */
+
+CREATE TABLE `tbarticle` (
+  `codart` int(11) NOT NULL COMMENT 'code d''article',
+  `nomart` varchar(15) NOT NULL COMMENT 'nom de l''article',
+  PRIMARY KEY (`codart`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `tbarticle` */
+
+/*Table structure for table `tbfamille` */
+
+CREATE TABLE `tbfamille` (
+  `codfam` int(11) NOT NULL COMMENT 'code de la famille',
+  `nomfam` varchar(15) NOT NULL COMMENT 'nom de la famille',
+  PRIMARY KEY (`codfam`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `tbfamille` */
+
+/*Table structure for table `tbsousfamille` */
+
+CREATE TABLE `tbsousfamille` (
+  `codsoufam` int(11) NOT NULL COMMENT 'code sous famille',
+  `nomsoufam` varchar(15) NOT NULL COMMENT 'nom sous famille',
+  PRIMARY KEY (`codsoufam`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `tbsousfamille` */
+
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
