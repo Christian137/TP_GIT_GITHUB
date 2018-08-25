c6420a1a496beab982593d28c18e72d517e42cc8
Christian <lowechristian1@yahoo.fr>
2018-08-25 11:21:48 +0100

    création des jointures entre les tables famille, sous famille et article

--- datastock.sql
+++ datastock.sql
@@ -18,30 +18,45 @@ USE `database`;
 
 /*Table structure for table `tbarticle` */
 
+DROP TABLE IF EXISTS `tbarticle`;
+
 CREATE TABLE `tbarticle` (
   `codart` int(11) NOT NULL COMMENT 'code d''article',
   `nomart` varchar(15) NOT NULL COMMENT 'nom de l''article',
-  PRIMARY KEY (`codart`)
+  `clécodsoufam` int(11) NOT NULL COMMENT 'clé code sous famille',
+  PRIMARY KEY (`codart`),
+  UNIQUE KEY `NewIndex1` (`nomart`),
+  KEY `indexsousfamille` (`clécodsoufam`),
+  CONSTRAINT `FK_tbarticle` FOREIGN KEY (`clécodsoufam`) REFERENCES `tbsousfamille` (`codsoufam`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*Data for the table `tbarticle` */
 
 /*Table structure for table `tbfamille` */
 
+DROP TABLE IF EXISTS `tbfamille`;
+
 CREATE TABLE `tbfamille` (
   `codfam` int(11) NOT NULL COMMENT 'code de la famille',
   `nomfam` varchar(15) NOT NULL COMMENT 'nom de la famille',
-  PRIMARY KEY (`codfam`)
+  PRIMARY KEY (`codfam`),
+  UNIQUE KEY `NewIndex1` (`nomfam`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*Data for the table `tbfamille` */
 
 /*Table structure for table `tbsousfamille` */
 
+DROP TABLE IF EXISTS `tbsousfamille`;
+
 CREATE TABLE `tbsousfamille` (
   `codsoufam` int(11) NOT NULL COMMENT 'code sous famille',
   `nomsoufam` varchar(15) NOT NULL COMMENT 'nom sous famille',
-  PRIMARY KEY (`codsoufam`)
+  `clecodfam` int(11) NOT NULL COMMENT 'cle code famille',
+  PRIMARY KEY (`codsoufam`),
+  UNIQUE KEY `NewIndex1` (`nomsoufam`),
+  KEY `indexcodfamille` (`clecodfam`),
+  CONSTRAINT `FK_tbsousfamille` FOREIGN KEY (`clecodfam`) REFERENCES `tbfamille` (`codfam`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*Data for the table `tbsousfamille` */
